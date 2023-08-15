8ac83ffe706f00bae47485641446e095def2320b
Jacob Smit <jacobsmit@shaw.ca>
2023-08-15 16:01:17 -0700

    added get, and post methods

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+app = express();
+const PORT = 8080;
+
+let roster = [];
+
+app.use(express.json())
+
+app.get("/api/roster", (req, res) => {
+    res.json(roster);
+    console.log(roster);
+});
+
+app.post("/api/roster/:id", (req, res) => {
+    roster.push(req.body);
+    res.status(201).json(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on " + PORT);
+});
