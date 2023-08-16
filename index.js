dc4f095cac42da092d814d76cb5f982b08ad69ec
Jacob Smit <jacobsmit@shaw.ca>
2023-08-15 17:24:16 -0700

    added put and delete methods

--- index.js
+++ index.js
@@ -10,12 +10,51 @@ app.use(express.json());
 
 app.get("/api/roster", (req, res) => {
     res.json(roster);
-    console.log(roster);
 });
 
 app.post("/api/roster/:id", (req, res) => {
-    roster.push(req.body);
-    res.status(201).json(req.body);
+    const newPlayer = {
+        id: req.params.id,
+        ... req.body
+    }
+    roster.push(newPlayer);
+    res.status(201).json(newPlayer);
+});
+
+app.put("/api/roster/:id", (req, res) => {
+    const updatedPlayer = {
+        id: req.params.id,
+        ... req.body
+    }
+    found = false;
+    roster.forEach((player, index) => {
+        if (player.id == req.params.id) {
+            roster[index] = updatedPlayer;
+            found = true;
+        }
+    });
+    if (found) {
+        res.status(200).json(updatedPlayer);
+    }    
+    else {
+        res.status(400).json( { message: `Player ${req.params.id} unable to be found successfully` } );
+    }
+});
+
+app.delete("/api/roster/:id", (req, res) => {
+    found = false;
+    roster.forEach((player, index) => {
+        if (player.id == req.params.id) {
+            roster.splice(index, 1);
+            found = true;
+        }
+    });
+    if (found) {
+        res.status(200).json( { message: `Player ${req.params.id} deleted successfully` } );
+    }
+    else {
+        res.status(400).json( { message: `Player ${req.params.id} unable to be found successfully` } );
+    }
 });
 
 app.listen(PORT, () => {
